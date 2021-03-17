7e5e8e427f0e1bf05b95c6063942dd3cc0bcb318
sherandrei <welldryus@gmail.com>
2021-03-17 23:29:33 +0300

    first test

--- include/LCS.hpp
+++ include/LCS.hpp
@@ -1,17 +1,19 @@
 #include <vector>
 #include <algorithm>
 
+namespace lcs {
+
 template<typename T,
-        std::enable_if_t<std::is_floating_point<T>::value, bool> = false>
+        std::enable_if_t<std::is_integral<T>::value, bool> = true>
 std::vector<T> LCS(const std::vector<T>& S1, const std::vector<T>& S2) {
     std::vector<std::vector<int>> table(S1.size() + 1,
                                     std::vector<int>(S2.size() + 1));
-    int i, j;
+    size_t i, j;
 
-    for (i = 0; i <= a.size(); ++i) {
-        for (j = 0; j <= b.size(); ++j) {
-            if (i == 0 || j == 0)
-                table[i][j] = 0;
+    for (i = 0u; i <= S1.size(); ++i) {
+        for (j = 0u; j <= S2.size(); ++j) {
+            if (i * j == 0u)
+                table[i][j] = 0u;
             else if (S1[i - 1] == S2[j - 1])
                 table[i][j] = table[i - 1][j - 1] + 1;
             else
@@ -19,19 +21,23 @@ std::vector<T> LCS(const std::vector<T>& S1, const std::vector<T>& S2) {
         }
     }
 
-    int index = table[S1.size()][S2.size()]
-    std::vector<T> LCS(index);
+    int index = table[S1.size()][S2.size()];
+    std::vector<T> lcs(index);
 
+    i = S1.size(), j = S2.size();
     while (i > 0 && j > 0) {
         if (S1[i - 1] == S2[j - 1]) {
-          LCS[index - 1] = S1[i - 1];
+          lcs[index - 1] = S1[i - 1];
           i--, j--, index--;
-        } else if (LCS_table[i - 1][j] > LCS_table[i][j - 1]) {
+        } else if (table[i - 1][j] > table[i][j - 1]) {
             i--;
         } else {
             j--;
         }
     }
 
-    return LCS;
+    return lcs;
 }
+
+}  // namespace lcs
+
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <vector>
+
+#include "test_runner.h"
 #include "LCS.hpp"
 
+void TestString();
+
+void TestString() {
+    std::vector<char> s1 = { 'A', 'C', 'A', 'D', 'B'};
+    std::vector<char> s2 = { 'C', 'Q', 'D', 'B', 'A'};
+    ASSERT_EQUAL(lcs::LCS<char>(s1, s2), std::vector<char>({'C', 'D', 'B'}));
+}
+
 int main() {
-    
+    TestRunner tr;
+    RUN_TEST(tr, TestString);
 }
