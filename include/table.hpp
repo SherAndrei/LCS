e760e27a552ef830d26d0273c8e09e4e89eb9492
sherandrei <welldryus@gmail.com>
2021-03-24 04:37:35 +0300

    BaseTable

--- include/table.hpp
+++ include/table.hpp
@@ -5,18 +5,39 @@
 
 namespace lcs {
 
-template<typename T,
-         std::enable_if_t<std::is_integral_v<T>, bool> = true>
-class IntegralTable {
- public:
-    using node_t = uint16_t;
-
- public:
-    IntegralTable(const std::vector<T>& S1,
-                  const std::vector<T>& S2)
+template<typename node_type>
+class BaseTable {
+ protected:
+    template<typename T>
+    BaseTable(const std::vector<T>& S1,
+              const std::vector<T>& S2)
         : _rows(S1.size() + 1)
         , _columns(S2.size() + 1)
-        , _table(std::make_unique<node_t[]>(_rows * _columns)) {
+        , _table(std::make_unique<node_type[]>(_rows * _columns)) {}
+
+ protected:
+    node_type& at(size_t i, size_t j) {
+        return _table.get()[i * _columns + j];
+    }
+
+ public:
+    node_type at(size_t i, size_t j) const {
+        return _table.get()[i * _columns + j];
+    }
+
+ protected:
+    const size_t _rows;
+    const size_t _columns;
+    std::unique_ptr<node_type[]> _table;
+};
+
+class IntegralTable final : public BaseTable<uint16_t> {
+ public:
+    template<typename T,
+         std::enable_if_t<std::is_integral_v<T>, bool> = true>
+    IntegralTable(const std::vector<T>& S1,
+                  const std::vector<T>& S2)
+        : BaseTable(S1, S2) {
             size_t i, j;
             for (i = 0u; i <= _rows - 1; ++i) {
                 for (j = 0u; j <= _columns - 1; ++j) {
@@ -29,21 +50,8 @@ class IntegralTable {
                 }
             }
         }
-
- private:
-    node_t& at(size_t i, size_t j) {
-        return _table.get()[i * _columns + j];
-    }
-
- public:
-    node_t at(size_t i, size_t j) const {
-        return _table.get()[i * _columns + j];
-    }
-
- private:
-    const size_t _rows;
-    const size_t _columns;
-    std::unique_ptr<node_t[]> _table;
 };
+
+// class FloatingTable
 
 }  // namespace lcs
